77c90d691b5d66d715d85bf3514b2c60f5ccdb7f
frazier56 <frazierlee@gmail.com>
2026-08-23 11:37:39 -0500

    Fix OneEvent UAT tools and tracking

--- notifySms-bWbnnkOo.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./index-ZA6JiyKq.js";function n(e){s.functions.invoke("notify-new-message",{body:{recipient_id:e.recipientId,sender_id:e.senderId,message_preview:e.messagePreview,message_type:e.messageType||"text"}}).catch(()=>{})}export{n};
