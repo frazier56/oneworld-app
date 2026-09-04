d6db37c239174f85e7274f8bd3a8561e040a3487
frazier56 <frazierlee@gmail.com>
2026-09-04 00:08:46 -0500

    Deploy durable password-change confirmation

---
+++ use-mobile-BbE8KayN.js
@@ -0,0 +1,11 @@
+import{$ as r,r as t}from"./index-Cg9ddvc0.js";/**
+ * @license lucide-react v1.30.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],d=r("circle-alert",i);/**
+ * @license lucide-react v1.30.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["rect",{width:"5",height:"5",x:"3",y:"3",rx:"1",key:"1tu5fj"}],["rect",{width:"5",height:"5",x:"16",y:"3",rx:"1",key:"1v8r4q"}],["rect",{width:"5",height:"5",x:"3",y:"16",rx:"1",key:"1x03jg"}],["path",{d:"M21 16h-3a2 2 0 0 0-2 2v3",key:"177gqh"}],["path",{d:"M21 21v.01",key:"ents32"}],["path",{d:"M12 7v3a2 2 0 0 1-2 2H7",key:"8crl2c"}],["path",{d:"M3 12h.01",key:"nlz23k"}],["path",{d:"M12 3h.01",key:"n36tog"}],["path",{d:"M12 16v.01",key:"133mhm"}],["path",{d:"M16 12h1",key:"1slzba"}],["path",{d:"M21 12v.01",key:"1lwtk9"}],["path",{d:"M12 21v-1",key:"1880an"}]],y=r("qr-code",o),h=768,s=512;function k(){const[n,c]=t.useState(!0);return t.useEffect(()=>{const e=()=>c(Math.min(window.innerWidth,s)<h);return e(),window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)},[]),n}export{d as C,y as Q,k as u};
